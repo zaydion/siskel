0d8dd3ee6993679c432ee40f4ff52a73c8daaff8
walterlatimer <walter@walterlatimer.com>
2015-06-30 23:05:44 -0400

    Add attr_reader

--- siskel.rb
+++ siskel.rb
@@ -1,12 +1,13 @@
-require 'httparty'
-
 class Siskel
+  require 'httparty'
+
+  attr_reader :title
 
   def initialize(title)
     movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
     @title = movie["Title"]
   end
-end 
+end
 
 
 
